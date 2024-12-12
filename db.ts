7d48e0ad5a501f0b3a0d537ea0b72dece5d1cd03
Claire Tsai <claire_tsai@horacemann.org>
2024-12-12 11:01:54 -0500

    Asdf

--- db.ts
+++ db.ts
@@ -4,6 +4,8 @@
 import path from "path";
 import {Request, Response} from 'express';
 import sqlite3 from "sqlite3";
+import { RequestHandler } from "express-serve-static-core";
+import { ParsedQs } from "qs";
 const sqlite = sqlite3.verbose();
 const dbFile = path.join(__dirname, "foo.db");
 // below is the line for vanilla ES6 js to work; not necessary with typescript
@@ -47,3 +49,58 @@ export const getUserById = (request: Request, response: Response) => {
 
 // ----- FILL IN BELOW -----
 // Write and export the rest of the functions needed by index.js!
+
+//get all users
+export const getAllUsers = (request: Request, response: Response) => {
+  const query = 'SELECT * FROM user';
+  db.all(query, (err, rows) => {
+    if (err){
+      console.error(err.message);
+      response.status(400).json({ error: err.message });
+      return;
+    }
+    response.status(200).json(rows);
+  });
+}
+
+//make a user
+export const createUser = (request: Request, response: Response) => {
+  const {name} = request.body;
+  const query = "INSERT INTO user (name) VALUES (?)";
+  db.run(query, [name], function (err) {
+    if (err) {
+      console.error(err.message);
+      response.status(400).json({ error: err.message });
+      return;
+    }
+    response.status(200).send("did it"); 
+  });
+}
+
+//update user
+export const updateUser = (request: Request, response: Response) => {
+  const id = parseInt(request.params.id);
+  const {name} = request.body;
+  const query = "UPDATE user SET name = ? WHERE id = ?" 
+  db.run(query, [name, id], function (err) {
+    if (err) {
+      console.error(err.message);
+      response.status(400).json({ error: err.message });
+      return;
+    }
+    response.status(200).send("did it"); 
+  });
+}
+//delete user
+export const deleteUser = (request: Request, response: Response) => {
+  const id = parseInt(request.params.id);
+  const query = "DELETE FROM user WHERE id = ?" 
+  db.run(query, [id], function (err) {
+    if (err) {
+      console.error(err.message);
+      response.status(400).json({ error: err.message });
+      return;
+    }
+    response.status(200).send("did it"); 
+  });
+}
